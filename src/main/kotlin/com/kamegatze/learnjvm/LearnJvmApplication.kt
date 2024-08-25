e570c66dc03b593b92d702af5112da1ea67beaf2
Shiryaev Aleksey <aleksi.aleksi2014@yandex.ru>
2024-08-25 22:29:02 +0300

    init

---
+++ src/main/kotlin/com/kamegatze/learnjvm/LearnJvmApplication.kt
@@ -0,0 +1,11 @@
+package com.kamegatze.learnjvm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnJvmApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearnJvmApplication>(*args)
+}
---
+++ src/test/kotlin/com/kamegatze/learnjvm/LearnJvmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kamegatze.learnjvm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearnJvmApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
